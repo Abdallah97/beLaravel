0029e22b5fa64361fa58e467ce8e0e4797428ed5
ariyantowbw <ariyantowbw@outlook.com>
2017-04-13 14:48:36 +0700

    Creates forms for users creation and testing it

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -10,6 +10,26 @@
     </div>
 
     <div class="form-group">
+        {!! Form::label('email', 'Email: ') !!}
+        {!! Form::email('email', null, ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('password', 'Password: ') !!}
+        {!! Form::password('password', ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('role_id', 'Role: ') !!}
+        {!! Form::select('role_id', $roles, 3, ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('is_active', 'Status: ') !!}
+        {!! Form::select('is_active', array(0 =>'Inactive', 1 => 'Active'), 0, ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
         {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
     </div>
     {!! Form::close() !!}
