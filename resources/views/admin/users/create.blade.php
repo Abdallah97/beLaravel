4f46d6633decaf33dc6e09b65f14d54d7428c475
ariyantowbw <ariyantowbw@outlook.com>
2017-04-13 15:49:49 +0700

    Include a blade to user creation pages to displays error messages

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -21,7 +21,7 @@
 
     <div class="form-group">
         {!! Form::label('role_id', 'Role: ') !!}
-        {!! Form::select('role_id', $roles, 3, ['class'=>'form-control']) !!}
+        {!! Form::select('role_id', [''=>'Choose a role..'] + $roles, null, ['class'=>'form-control']) !!}
     </div>
 
     <div class="form-group">
@@ -33,4 +33,14 @@
         {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
     </div>
     {!! Form::close() !!}
+
+    {{--Showing errors using include instead--}}
+    {{--@if(count($errors) > 0)--}}
+        {{--@foreach($errors->all() as $error)--}}
+            {{--<div class="alert alert-danger">--}}
+                {{--{{$error}}--}}
+            {{--</div>--}}
+        {{--@endforeach--}}
+    {{--@endif--}}
+    @include('include.form-error')
 @stop
