09e3b6cee259c132d953aa20ad2d59707572c74b
ariyantowbw <ariyantowbw@outlook.com>
2017-04-14 11:53:41 +0700

    Finishes the edit user page, create a request for it, doing some tests

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserEditRequest;
 use App\Http\Requests\UsersRequest;
 use App\Photo;
 use App\Role;
@@ -100,9 +101,27 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UserEditRequest $request, $id)
     {
         //
+        $user = User::findOrFail($id);
+        $input = $request->all();
+
+        if($file = $request->file('image')){
+            $name = time().$file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['path'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+        }
+
+        $input['password'] = bcrypt($request->password);
+
+        $user->update($input);
+
+        return redirect('/admin/users');
     }
 
     /**
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -24,7 +24,7 @@
                         <td><img height="46" width="46" src="{{$user->photo->path or '/images/300px-No_image_available.svg.png'}}" alt=""><img src="" alt=""></td>
                         <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
                         <td>{{$user->email}}</td>
-                        <td>{{$user->role->name}}</td>
+                        <td>{{$user->role->name or 'User has no role'}}</td>
                         <td>{{$user->is_active === 1 ? 'Active':'Inactive'}}</td>
                         <td>
                             {{$user->created_at}}<br>
