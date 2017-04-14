e992e3263ba401dffb057872c7e78211740f94d8
ariyantowbw <ariyantowbw@outlook.com>
2017-04-14 14:59:45 +0700

    Create a 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <h1 class="text-center">
+                        This aren't the pages you're looking for!
+                    </h1>
+                    <img src="/images/404.jpg" alt="Imagine Obi-Wan Kenobi pulling your minddoorknob" class="center-block">
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
