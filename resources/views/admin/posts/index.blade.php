d0e253009e6f990fd59c9b159fd49137a1c3c6ad
ariyantowbw <ariyantowbw@outlook.com>
2017-04-17 10:32:22 +0700

    Create a table and model for post; create its relationship with users and photos table; displaying posts' table content in posts' index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,4 +2,37 @@
 
 @section('content')
     <h1>Posts</h1>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Writer</th>
+            <th>Title</th>
+            <th>Content</th>
+            <th>Created at</th>
+            <th>Updated at</th>
+        </tr>
+        </thead>
+        <tbody>
+        @if($posts)
+            @foreach($posts as $post)
+                <tr>
+                    <td>{{$post->id}}</td>
+                    <td>{{$post->user->name}}</td>
+                    <td>{{$post->title}}</td>
+                    <td>{{$post->content}}</td>
+                    <td>
+                        {{$post->created_at}}<br>
+                        <small>({{$post->created_at->diffForHumans()}})</small>
+                    </td>
+                    <td>
+                        {{$post->updated_at}}<br>
+                        <small>({{$post->updated_at->diffForHumans()}})</small>
+                    </td>
+                </tr>
+            @endforeach
+        @endif
+        </tbody>
+    </table>
 @stop
